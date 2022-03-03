3808d9d788f2f942bb20d3982aea081f7ce86de4
dseng13 <dseng13@gmail.com>
2022-03-02 20:41:13 -0500

    change dl tag to ul tag for bulleted specs list

--- client/components/gpu-listings/GpuCard.js
+++ client/components/gpu-listings/GpuCard.js
@@ -18,13 +18,13 @@ export default function GpuCard() {
       <div>
         <span className={style.brand}>ASUS</span>
         <h3 className={style.title}>ASUS ROG Strix NVIDIA GeForce RTX 3080 Ti OC Edition Gaming Graphics Card</h3>
-        <dl className={style.specs}>
+        <ul className={style.specs}>
           <li>PCI Express x16</li>
           <li>12 GB</li>
           <li>GDDR6X</li>
           <li>384 bits</li>
           <li>4000 MHz</li>
-        </dl>
+        </ul>
         <hr className={style.divider} />
         <div className={style.footer}>
           <span className={style.price}>
