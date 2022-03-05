8cc4f31db5cdcb7c91ba847159c45713969daa31
dseng13 <dseng13@gmail.com>
2022-03-05 14:52:24 -0500

    updated gpu listings to display cards based on dummy data

--- client/components/gpu-listings/GpuListings.js
+++ client/components/gpu-listings/GpuListings.js
@@ -1,11 +1,27 @@
 import GpuCard from './GpuCard'
 
+import gpuData from '../../../api/data/data.json'
+
 import style from './GpuListings.module.css'
 
 export default function GpuListings() {
   return (
     <main className={style.wrapper}>
-      <GpuCard />
+      {gpuData.map(gpu =>
+        <GpuCard
+          key={gpu.id}
+          mhz={gpu.MHz}
+          bits={gpu.bits}
+          brand={gpu.brand}
+          gddr={gpu.gddr}
+          id={gpu.id}
+          img={gpu.img}
+          price={gpu.price}
+          slots={gpu.slots}
+          title={gpu.title}
+          vram={gpu.vram}
+        />
+      )}
     </main>
   )
 }
--- client/next.config.js
+++ client/next.config.js
@@ -3,6 +3,9 @@ const withSvgr = require("next-svgr");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['c1.neweggimages.com']
+  }
 }
 
 module.exports = withSvgr(nextConfig)
